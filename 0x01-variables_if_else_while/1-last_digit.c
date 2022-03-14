5756d9c676befff25dad23a38928a193cf1c03b6
root <godsonchuks201@gmail.com>
2022-03-14 02:25:19 -0700

    now

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,22 +4,26 @@
 /* more headers goes there */
 
 /**
- * main - entry point
+ * main - Program entry point
  *
- * Return: 0 if success
+ * Return: 0 on success. Error code otherwise
  */
 int main(void)
 {
-int n;
+  int n, lastDigit;
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-/* your code goes there */
-if (n > 0)
-printf("Last digit of %d and is greater than 5\n", n, lastDigit);
-else if (n == 0)
-printf("Last digit of %d is %d and is 0\n", n, lastDigit);
-else
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
+  srand(time(0));
+  n = rand() - RAND_MAX / 2;
+  lastDigit = n % 10;
+  /* your code goes there */
+  if (lastDigit > 5)
+    printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
+  else if (lastDigit == 0)
+    printf("Last digit of %d is %d and is 0\n", n, lastDigit);
+  else if (lastDigit < 6 && lastDigit != 0)
+    {
+      printf("Last digit of %d is %d and is less than 6 and ", n, lastDigit);
+      printf("not 0\n");
+    }
 return (0);
 }
