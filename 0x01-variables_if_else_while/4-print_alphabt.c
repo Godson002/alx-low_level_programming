995d2cd54475cd341172a3c7d35aa89f1c3bee64
root <godsonchuks201@gmail.com>
2022-03-14 03:58:18 -0700

    how far

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,48 +2,19 @@
 
 int main(void)
 {
-  char ch = 'a';
-  while(ch <= 'z')
-    {
-      if (ch != 'e' && ch != 'q')
-	{
-	  putchar(ch);
-	}
-      ch++;
-    }
-  putchar('\n');
-
-  return 0;
-}
-
-#include <stdio.h>
-
-int main(void)
+char ch = 'a';
+while(ch <= 'z')
 {
-    
-  for (char ch = 'a'; ch <= 'z'; ch++)
-    {
-      if (ch != 'e' && ch != 'q')
-	{
-	  putchar(ch);
-	}
-    }
-  putchar('\n');
-
-  return 0;
-}
-
-int main(void)
+if (ch != 'e' && ch != 'q')
 {
-    
-  for (char ch ='a'; ch <= 'z'; ch++)
-    {
-      if (ch == 'e' || ch == 'q')
-	ch++;
-      putchar(ch);
-      
-    }
-  putchar('\n');
-
-  return 0;
+putchar(ch);
 }
+ch++;
+}
+putchar('\n');
+
+return 0;
+}
+
+
+
