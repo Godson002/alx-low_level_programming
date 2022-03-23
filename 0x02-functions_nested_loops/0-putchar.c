605a8136592326b4263a2b8e604be49af71d4b5e
root <godsonchuks201@gmail.com>
2022-03-23 01:04:28 -0700

    now

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,13 +9,13 @@
 int main(void)
 {
 int n = 0;
-char str_hbt[9] = "Putchar";
+char str_hbt[9] = "_putchar";
 
 while (n < 9)
 {
-putchar(str_hbt[n]);
+_putchar(str_hbt[n]);
 n += 1;
 }
-putchar('\n');
+_putchar('\n');
 return (0);
 }
