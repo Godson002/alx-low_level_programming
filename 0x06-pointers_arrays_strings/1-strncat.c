873183f6d112e174feffafece8711e57e9fabba2
root <godsonchuks201@gmail.com>
2022-03-24 04:33:52 -0700

    second half commit

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -7,7 +7,7 @@
  * @src: s2
  * Return: Always 0.
  */
-char *strncat(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
 
 int i = 0, c = 0, s = 0;
@@ -17,7 +17,7 @@ while (dest[i] != '\0')
 i++;
 }
 while (src[c] != '\0')
-    {
+{
 c++;
 }
 
